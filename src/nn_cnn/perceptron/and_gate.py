ebd74a51b40139bdd0d4e00b57beab71c66de9a3
shisotem <129283841+shisotem@users.noreply.github.com>
2024-07-12 13:28:01 +0900

    feat: ANDゲートをbiasを用いて実装

--- src/nn_cnn/perceptron/and_gate.py
+++ src/nn_cnn/perceptron/and_gate.py
@@ -2,10 +2,20 @@
 # Point: 同じ構造のパーセプトロンが、パラメータを調整することで、ANDにもNANDにもORにも変身できる
 
 
+# def AND(x1, x2):
+#     w1, w2, theta = 0.5, 0.5, 0.7
+#     y = x1 * w1 + x2 * w2
+#     return 0 if y <= theta else 1
+
+import numpy as np
+
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    y = x1 * w1 + x2 * w2
-    return 0 if y <= theta else 1
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7  # thetaを左辺に移項、-thetaをbと置く (バイアスは発火のしやすさを制御する)
+    y = np.sum(w * x) + b
+    return 0 if y <= 0 else 1
 
 
 print(AND(0, 0))
