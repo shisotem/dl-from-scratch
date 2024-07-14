34f8db31612c806c70036c1d0993983e35665fec
shisotem <129283841+shisotem@users.noreply.github.com>
2024-07-15 02:13:53 +0900

    refactor: avoid overflow

--- src/nn_cnn/nn/softmax.py
+++ src/nn_cnn/nn/softmax.py
@@ -2,13 +2,19 @@ import numpy as np
 
 
 def softmax(x: np.ndarray) -> np.ndarray:
-    exp_x = np.exp(x)
-    y = exp_x / np.sum(exp_x)
+    """
+    y = exp(x_k) / Σexp(x_i)
+    = (exp(c) * exp(x_k)) / (exp(c) * Σexp(x_i))
+    = exp(x_k + c) / Σexp(x_i + c)
+    (cは任意の定数: overflow対策)
+    """
+    c = np.max(x)
+    y = np.exp(x - c) / np.sum(np.exp(x - c))
     return y
 
 
 if __name__ == "__main__":
-    x = np.array([0.3, 2.9, 4.0])
+    x = np.array([1010, 1000, 990])  # 通常の実装ではoverflowしてしまう
     y = softmax(x)
     print(y)
     print(np.sum(y))
