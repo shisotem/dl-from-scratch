5e25b983eb48ba6fd9143b2b3981a8bc9d385ec6
shisotem <129283841+shisotem@users.noreply.github.com>
2024-07-14 17:27:41 +0900

    feat: add step function

---
+++ src/nn_cnn/nn/step_function.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def step_function(x: np.ndarray):
+    return np.array(x > 0, dtype=int)
+
+
+if __name__ == "__main__":
+    x = np.arange(-5, 5.1, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
